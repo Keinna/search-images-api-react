aa6a1ed75160ffd0c5f0ddbdff94f366e4fc8407
Anniek <anniekgithub@gmail.com>
2023-05-08 22:10:11 +0200

    run build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import ImageList from "./components/ImageList";
+import Background from "./components/Background";
 import searchImages from "./api";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <div>
             <SearchBar onSubmit={handleSubmit} />
             <ImageList images={images} />
+            {images.length === 0 && <Background />}
         </div>
     );
 }
---
+++ src/components/Background.js
@@ -0,0 +1,12 @@
+import "./Background.css";
+
+function Background() {
+    return (
+        <div className="background-container">
+            <div className="circle"></div>
+            <div className="square"></div>
+        </div>
+    );
+}
+
+export default Background;
